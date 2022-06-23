1a1c10adb2336b62913761b30a6af2090b6763c7
Andrew Farenyuk <a.farenyuk@icloud.com>
2022-06-23 19:39:55 -0400

    Created Client class to encapsulate client data and behaviour. Added initial test case.

--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -25,11 +25,6 @@ public class Main {
 
         // References to database files
         final Path dbPath = Path.of("C:\\Users\\andre\\Documents\\repos\\Client-Server-Simulation\\src\\main\\java\\server\\data\\db.json");
-//        final Path dbPath = Path.of("./src/server/data/db.json");
-
-//        System.out.println(Files.exists(dbPath));
-//        System.out.println(System.getProperty("user.dir"));
-//    final Path dbPath = Path.of("C:\\Users\\andre\\IdeaProjects\\JSON Database\\task\\src\\server\\data\\db.json");
 
         boolean isRunning = true;
 
@@ -222,3 +217,9 @@ public class Main {
         System.exit(0);
     }
 }
+
+//        final Path dbPath = Path.of("./src/server/data/db.json");
+
+//        System.out.println(Files.exists(dbPath));
+//        System.out.println(System.getProperty("user.dir"));
+//    final Path dbPath = Path.of("C:\\Users\\andre\\IdeaProjects\\JSON Database\\task\\src\\server\\data\\db.json");
